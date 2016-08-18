93b3ded1aa23e186f18d90eeb4716d35a2fbcc1b
Tarcisio <howtomakeacheap@outlook.com>
2016-08-18 19:55:52 -0300

    Commit inicial

---
+++ AdminResearch/default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="AdminResearch._default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolderHead" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceContainer" runat="server">
+    <p>oi</p>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolderFooter" runat="server">
+</asp:Content>
---
+++ AdminResearch/handlers/Default.ashx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Security;
+
+namespace AdminResearch.handlers
+{
+    /// <summary>
+    /// Summary description for Default
+    /// </summary>
+    public class Default : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
